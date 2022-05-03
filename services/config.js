0d8ed8fd3010855a9464f949ea9d913f9903df37
Carlos Guillen <carlos_guillenm_@hotmail.com>
2022-05-03 21:38:03 +0200

    Stopped tracking config.js File

--- services/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {  
-    consumer_key: 'kDwQqV7Ii4K4u1fQ1XeH9vzmn' ,  
-    consumer_secret: 'lfrDPVfnlAcTR4r9SMPreVbqzLrP7C7kcfqb2aYQCGqQgMqZL2',  
-    access_token_key: '1518904631409221632-Jy7XQ7cUqTuWWy8uoFDnJk2i4TfsbM',  
-    access_token_secret: 'yDxkZk6R0FMlca9k36ZEIYcO3SuhugTtYGbVnZ48OOsBk'  
-  }
